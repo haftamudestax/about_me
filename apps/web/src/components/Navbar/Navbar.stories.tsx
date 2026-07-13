078a069205ba9e1f01c18362d0631e1e3ad51bbc
haftamudesta <desta451616@hotmail.com>
2026-07-13 12:22:29 -0700

    Fix lint errors

--- apps/web/src/components/Navbar/Navbar.stories.tsx
+++ apps/web/src/components/Navbar/Navbar.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { expect, within, userEvent } from "@storybook/test";
 import { Navbar } from "./Navbar";
 
